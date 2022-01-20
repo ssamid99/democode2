22695bdf020e90a382ab437fa564c4e2ef8de9ec
Samid <samidms@code.edu.az>
2022-01-21 01:12:29 +0400

    mms

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace ConsoleApp.VariablesOperatorsAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int phone_price = 900; //sneak_case
+            int phoneName = 5; //camelCase
+            int PhoneRamSlots = 1; //PascalCase
+            */
+            /*
+            double pi = 3.14;
+            Console.WriteLine(pi);
+
+            Console.ReadKey();
+            */
+            //int a = 10;
+            //int b = 8;
+            //int c = a + b;
+            //int c = a - b;
+            //int c = a * b;
+            //int c = a / b;
+            //int c = a % b;  
+
+            //Console.WriteLine(c);
+
+            //  tapshiriq 3.
+            //int a = 11;
+            //int b = 13;
+            //int c = 2;
+            //int d = 3;
+            //
+            //int e = a + b;
+            //Console.WriteLine(e);   
+            //
+            //int f = c * d;
+            //Console.WriteLine(f);  
+            //
+            //int g = e / f;
+            //Console.WriteLine(g);
+
+            //Console.WriteLine(( a + b ) / ( c * d ));
+            //int a = 88;
+            //
+            //if (a % 2 == 0) ;
+            //{
+            //    Console.WriteLine("Cutdur")
+            //}
+            //
+            //if (a % 3 == 0) ;
+            //{
+            //    Console.WriteLine("Tekdir");
+            //}
+
+            //  tapshiriq 2.
+            //int a = 23;
+            //
+            //
+            //int b = a % 4;
+            //
+            //    if (b == 0)
+            //{
+            //    Console.WriteLine("Eded 4-e bolunur");
+            //}
+            //
+            //else
+            //{
+            //    Console.WriteLine("Eded 4-e bolunmur");
+            //}
+
+            //tapshiriq 1.
+            //int a = 73;
+            //int b = a % 2;
+            //
+            //if (b == 0)
+            //{
+            //    Console.WriteLine("Cutdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Tekdir");
+            //}
+
+            //    tapshiriq 4.
+
+            //Console.WriteLine("1-50 arasi tek ededler");
+            //for (int a = 1; a <= 50; a += 2)
+            //{
+            //    Console.WriteLine(a);
+            //    
+            //}
+
+            //int r = 3;
+            //int g = 5;
+                                                  //   tapshiriq 5.
+            
+            {
+                for (int i = 1; i <= 50; i++)
+                {
+                    if (i % 3 == 0 || i % 5 == 0)
+
+                    {Console.WriteLine(i);}
+                }
+
+            }
+
+            
+
+
+            
+
+
+
+
+
+
+            
+        }
+    }
+}
