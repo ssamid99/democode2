88d4d963723c9b6e5f61e0e1e624af0f3f1c8d93
ssamid99 <samidms.@code.edu.az>
2022-01-30 19:02:34 +0400

    ll

---
+++ ConsoleApp-samid/2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 student1 = new Class2();
+            student1.Name = "Ali";
+            student1.Catagory = "Ehmedov";
+            student1.Area = 23;
+            student1.Price = 12;
+            Class2 student2 = new Class2();
+            student2.Name = "Al";
+            student2.Catagory = "Ehmedov";
+            student2.Area = 23;
+            student2.Price = 23;
+            Class2 student3 = new Class2();
+            student3.Name = "li";
+            student3.Catagory = "Ehmedov";
+            student3.Area = 23;
+            student3.Price = 34;
+            Class2 student4 = new Class2();
+            student4.Name = "Ai";
+            student4.Catagory = "Ehmedov";
+            student4.Area = 23;
+            student4.Price = 45;
+
+            Class2[] students = new Class2[4];
+            students[0] = student1;
+            students[1] = student2;
+            students[2] = student3;
+            students[3] = student4;
+
+            foreach (var x in students)
+            {
+                x.GetTwo();
+                Console.WriteLine("-------------");
+            }
+        }
+    }
+}
---
+++ ConsoleApp-samid/Class2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_
+{
+    class Class2
+    {
+        public string Name;
+        public string Catagory;
+        public int Area;
+        public int NumberOfRooms;
+        public int Price;
+
+        public static object array { get; private set; }
+
+        /* public void GetInfo()
+         {
+             Console.WriteLine($"{Name}\n" +
+                 $"{Catagory}\n" +
+                 $"{Area}\n" +
+                 $"{NumberOfRooms}\n" +
+                 $"{Price}");
+
+
+
+
+
+         }*/
+        public static string GetTwo(int x, int y, Class2[] array) => $"{array[x].Name} {array[x].Catagory} {array[x].Area} {array[x].NumberOfRooms} {array[x].Price}";
+
+
+
+
+
+
+
+
+
+
+    }
+}
